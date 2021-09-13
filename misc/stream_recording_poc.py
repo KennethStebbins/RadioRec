8a4dadbcca7e110f1aec37730aa1ac273cbed9f9
Kenneth Stebbins <contact@kennethstebbins.com>
2021-09-12 19:14:01 -0600

    Added stream recording proof of concept

---
+++ misc/stream_recording_poc.py
@@ -0,0 +1,22 @@
+import requests, sys
+
+URL = 'https://14543.live.streamtheworld.com/KMGLFMAAC.aac?pname=StandardPlayerV4&pversion=4.19.2-041&dist=triton-web&tdsdk=js-2.9&banners=300x250&sbmid=f9b251b5-dda4-402a-b67c-4c52e64b0dfb'
+# URL = get_stream_url()
+
+try:
+    req = requests.request('GET', URL, stream=True)
+
+    counter = 0
+    allTheBytes = b''
+    for b in req.iter_content():
+        allTheBytes += b
+        counter += 1
+        if counter > 145632:
+            break
+        
+
+    with open('./recorded.aac', 'wb') as f:
+        f.write(allTheBytes)
+finally:
+    # req.close()
+    pass
