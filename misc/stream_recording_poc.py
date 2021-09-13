dc831850acd7d613e9f1daf0bb8c3b303e32d826
Kenneth Stebbins <contact@kennethstebbins.com>
2021-09-12 19:45:57 -0600

    Using 'with' now to make things cleaner

--- misc/stream_recording_poc.py
+++ misc/stream_recording_poc.py
@@ -3,10 +3,7 @@ import requests, sys
 URL = 'https://14543.live.streamtheworld.com/KMGLFMAAC.aac?pname=StandardPlayerV4&pversion=4.19.2-041&dist=triton-web&tdsdk=js-2.9&banners=300x250&sbmid=f9b251b5-dda4-402a-b67c-4c52e64b0dfb'
 # URL = get_stream_url()
 
-try:
-    req = None
-    req = requests.request('GET', URL, stream=True)
-
+with requests.request('GET', URL, stream=True) as req:
     counter = 0
     allTheBytes = b''
     for b in req.iter_content():
@@ -18,7 +15,3 @@ try:
 
     with open('./misc/output/recorded.aac', 'wb') as f:
         f.write(allTheBytes)
-finally:
-    if req != None:
-        req.close()
-    pass
