4090da736141441dbc3b2f6a838be510b8851a9a
Kenneth Stebbins <contact@kennethstebbins.com>
2021-09-18 13:51:03 -0600

    Fixed some assertions and added basic read tests

--- tests/test_byte_buffer.py
+++ tests/test_byte_buffer.py
@@ -58,7 +58,7 @@ class TestByteBufferOversizedAppend(unittest.TestCase):
     
     def test_oversized_append(self):
         data = b'123456789AB'
-        expected = b'B23456789A'
+        expected = b'23456789AB'
 
         self.bb.append(data)
 
@@ -71,13 +71,55 @@ class TestByteBufferOversizedOverflowAppend(unittest.TestCase):
         self.bb = ByteBuffer(10)
         self.bb.append(b'PREPARED')
     
-    def test_oversized_append(self):
+    def test_oversized_overflow_append(self):
         data = b'123456789AB'
-        expected = b'3456789AB2'
+        expected = b'456789AB23'
 
         self.bb.append(data)
 
         self.assertSequenceEqual(self.bb.byte_array, expected)
+
+class TestByteBufferNormalRead(unittest.TestCase):
+    bb = None
+
+    def setUp(self) -> None:
+        self.bb = ByteBuffer(10)
+        self.bb.append(b'LUZ_NOCEDA')
+    
+    def test_normal_read(self):
+        expected = b'LUZ_NOCEDA'
+
+        result = self.bb.read()
+
+        self.assertSequenceEqual(result, expected)
+
+class TestByteBufferWraparoundRead(unittest.TestCase):
+    bb = None
+
+    def setUp(self) -> None:
+        self.bb = ByteBuffer(10)
+        self.bb.append(b'AMITYBLIGHT')
+    
+    def test_wraparound_read(self):
+        expected = b'MITYBLIGHT'
+
+        result = self.bb.read()
+
+        self.assertSequenceEqual(result, expected)
+
+class TestByteBufferOversizedRead(unittest.TestCase):
+    bb = None
+
+    def setUp(self) -> None:
+        self.bb = ByteBuffer(10)
+        self.bb.append(b'OWLLADYEDA')
+    
+    def test_oversized_read(self):
+        with self.assertRaises(ValueError) as cm:
+            self.bb.read(11)
+
+        self.assertEquals(cm.exception.args[0], 
+            'Requested length is longer than the buffer length')
 
 if __name__ == '__main__':
     unittest.main()
