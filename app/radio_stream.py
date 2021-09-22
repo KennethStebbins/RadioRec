26e09b462c1dd7305df2e0f5b1c51bebce8850b6
Kenneth Stebbins <contact@kennethstebbins.com>
2021-09-22 08:48:52 -0600

    Added passthrough args for super + retries for get_stream_url()

--- app/radio_stream.py
+++ app/radio_stream.py
@@ -1,19 +1,29 @@
-from requests import request, Response
-from selenium.webdriver.common.by import By
+import logging
+from requests import request
+from selenium.common.exceptions import TimeoutException
 from urllib3.response import HTTPResponse
 from threading import Thread
 
 from app.get_stream_url import get_stream_url
 from app.byte_buffer import ByteBuffer
 
+log = logging.getLogger('RadioRec')
+
 class RadioStream(Thread):
     byte_buffer : ByteBuffer = None
     url : str = None
 
-    def __init__(self, buffer_size : int = 307200) -> None:
+    def __init__(self, buffer_size : int = 307200, attempts : int = 3, *args, **kwargs) -> None:
         self.byte_buffer = ByteBuffer(buffer_size)
-        self.url = get_stream_url()
-        super().__init__()
+        for i in range(0, attempts):
+            try:
+                self.url = get_stream_url()
+                if self.url is not None:
+                    break
+            except TimeoutException:
+                log.debug(f'Failed to get stream URL: Attempt #{i + 1}')
+
+        super().__init__(*args, **kwargs)
 
     def run(self):
         with request('GET', self.url, stream=True) as r:
