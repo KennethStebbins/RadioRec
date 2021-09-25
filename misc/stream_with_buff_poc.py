e822c86c8bd06e0e6e962611887affec3fe13bea
Kenneth Stebbins <contact@kennethstebbins.com>
2021-09-25 13:14:30 -0600

    Various improvements
    - Changed logging calls to INFO, raised logging level.
    - Now writes pre- and post- sync files, in addition to post-wait files

--- misc/stream_with_buff_poc.py
+++ misc/stream_with_buff_poc.py
@@ -4,7 +4,7 @@ from app.radio_stream import RadioStream
 import logging
 
 log = logging.getLogger('RadioRec')
-log.setLevel(logging.DEBUG)
+log.setLevel(logging.INFO)
 
 consoleHandler = logging.StreamHandler(sys.stdout)
 consoleHandler.setLevel(logging.DEBUG)
@@ -12,58 +12,94 @@ consoleHandler.setLevel(logging.DEBUG)
 log.addHandler(consoleHandler)
 
 def go() -> None:
-    log.debug('Creating stream one')
-    startingTime = time.time()
+    log.info('Creating stream one')
     streamOne = RadioStream(daemon=True)
-    endingTime = time.time()
-    log.debug(f'Stream one created. Took {endingTime-startingTime} seconds.')
+    log.info('Stream one created')
+    log.info(f"Stream one URL: {streamOne.url}")
     streamOne.start()
-    log.debug('Started stream one')
+    log.info('Started stream one')
 
     # Introduce a de-sync
-    log.debug('Waiting 10 seconds...')
+    log.info('Waiting 10 seconds...')
     time.sleep(10)
-    log.debug('Done waiting 10 seconds...')
+    log.info('Done waiting 10 seconds...')
 
-    log.debug('Creating stream two')
+    log.info('Creating stream two')
     streamTwo = RadioStream(daemon=True)
-    log.debug('Stream two created')
+    log.info('Stream two created')
+    log.info(f"Stream two URL: {streamTwo.url}")
     streamTwo.start()
-    log.debug('Started stream two')
+    log.info('Started stream two')
 
     # Skip the pre-roll ad
-    log.debug('Waiting 45 seconds...')
+    log.info('Waiting 45 seconds...')
     time.sleep(45)
-    log.debug('Done waiting 45 seconds...')
+    log.info('Done waiting 45 seconds...')
+
+    # Pre-sync
+    log.info('Reading stream 1')
+    s1res = streamOne.byte_buffer.read(consume=False)
+    log.info('Done reading stream 1')
+    with open('./misc/output/s1rec-pre.aac', 'wb') as f:
+        log.info('Writing pre-sync stream 1')
+        f.write(s1res)
+        log.info('Done pre-sync writing stream 1')
+
+    log.info('Reading stream 2')
+    s2res = streamTwo.byte_buffer.read(consume=False)
+    log.info('Done reading stream 2')
+    with open('./misc/output/s2rec-pre.aac', 'wb') as f:
+        log.info('Writing pre-sync stream 2')
+        f.write(s2res)
+        log.info('Done pre-sync writing stream 2')
 
     syncBytes = streamOne.byte_buffer.readFromEnd(50000, consume=False)
     syncBytes = syncBytes[:47000]
-    log.debug('Seeking to sync bytes on stream two...')
+    log.info('Seeking to sync bytes on stream two...')
     streamTwo.byte_buffer.seekToSequence(syncBytes)
-    log.debug('Seeking to sync bytes on stream one...')
+    log.info('Seeking to sync bytes on stream one...')
     streamOne.byte_buffer.seekToSequence(syncBytes)
-    
-    log.debug('Waiting 90 seconds...')
-    time.sleep(90)
-    log.debug('Done waiting 90 seconds...')
 
-    log.debug('Reading stream 1')
-    s1res = streamOne.byte_buffer.read()
-    log.debug('Done reading stream 1')
-    with open('./misc/output/s1rec.aac', 'wb') as f:
-        log.debug('Writing stream 1')
+    # Post-sync
+    log.info('Reading stream 1')
+    s1res = streamOne.byte_buffer.read(consume=False)
+    log.info('Done reading stream 1')
+    with open('./misc/output/s1rec-post.aac', 'wb') as f:
+        log.info('Writing post-sync stream 1')
         f.write(s1res)
-        log.debug('Done writing stream 1')
+        log.info('Done post-sync writing stream 1')
 
-    log.debug('Reading stream 2')
-    s2res = streamTwo.byte_buffer.read()
-    log.debug('Done reading stream 2')
-    with open('./misc/output/s2rec.aac', 'wb') as f:
-        log.debug('Writing stream 2')
+    log.info('Reading stream 2')
+    s2res = streamTwo.byte_buffer.read(consume=False)
+    log.info('Done reading stream 2')
+    with open('./misc/output/s2rec-post.aac', 'wb') as f:
+        log.info('Writing post-sync stream 2')
         f.write(s2res)
-        log.debug('Done writing stream 2')
+        log.info('Done post-sync writing stream 2')
     
-    log.debug('Done!')
+    log.info('Waiting 90 seconds...')
+    time.sleep(90)
+    log.info('Done waiting 90 seconds...')
+
+    # Post-wait
+    log.info('Reading stream 1')
+    s1res = streamOne.byte_buffer.read()
+    log.info('Done reading stream 1')
+    log.info('Reading stream 2')
+    s2res = streamTwo.byte_buffer.read()
+    log.info('Done reading stream 2')
+
+    with open('./misc/output/s1rec-pw.aac', 'wb') as f:
+        log.info('Writing post-wait stream 1')
+        f.write(s1res)
+        log.info('Done writing post-wait stream 1')
+
+    with open('./misc/output/s2rec-pw.aac', 'wb') as f:
+        log.info('Writing post-wait stream 2')
+        f.write(s2res)
+        log.info('Done writing post-wait stream 2')
+    
+    log.info('Done!')
 
 
 go()
