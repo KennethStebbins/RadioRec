62ec4c7f2d096b3b5bdb281b54b1f45ad915103f
Kenneth Stebbins <contact@kennethstebbins.com>
2021-10-09 09:00:21 -0600

    Various improvements
    # RadioStream
    - RadioStream now uses proper convention to denote private methods and variables
    - RadioStream can now accept an int `preroll_len` at init
    - RadioStream will now throw an exception if it runs out of attempts to get a stream URL
    - RadioStream now dumps the first self._preroll_len bytes when opening a stream
    - Added read-only properties for byte_buffer and url in RadioStream
    
    # Added RadioStreamManager
    This class creates multiple RadioStreams and monitors them. It will denote one stream as the primary stream. If a non-primary stream fails, it is removed and a new one created to fill its space. If the primary stream fails, it is replaced by a functioning non-primary stream. If all streams fail, a new stream is created and used as the primary stream, then the non-primary streams are recreated.
    
    When the primary stream is replaced, RadioStreamManager invokes all its on_stream_failover_handler callables with the old primary and the new primary streams as arguments.
    
    # Added RedundantRadioStream
    Uses a RadioStreamManager to start multiple streams and write the primary's contents to its own buffer. If the primary fails, it syncs the new primary as best it can.

--- app/radio_stream.py
+++ app/radio_stream.py
@@ -1,8 +1,10 @@
 import logging
+from os import devnull
 from requests import request
 from selenium.common.exceptions import TimeoutException
 from urllib3.response import HTTPResponse
-from threading import Thread
+from threading import Event, RLock, Thread
+from typing import Callable, List
 
 from app.get_stream_url import get_stream_url
 from app.byte_buffer import ByteBuffer
@@ -10,25 +12,211 @@ from app.byte_buffer import ByteBuffer
 log = logging.getLogger('RadioRec')
 
 class RadioStream(Thread):
-    byte_buffer : ByteBuffer = None
-    url : str = None
+    _byte_buffer : ByteBuffer = None
+    _url : str = None
+    _preroll_len : int = 63500
+    
+    @property
+    def byte_buffer(self):
+        return self._byte_buffer
+    
+    @property
+    def url(self):
+        return self._url
 
-    def __init__(self, buffer_size : int = 307200, attempts : int = 3, *args, **kwargs) -> None:
-        self.byte_buffer = ByteBuffer(buffer_size)
+    def __init__(self, buffer_size : int = 307200, attempts : int = 3, preroll_len : int = 63500,
+            *args, **kwargs) -> None:
+        self._byte_buffer = ByteBuffer(buffer_size)
+        self._preroll_len = preroll_len
         for i in range(0, attempts):
             try:
-                self.url = get_stream_url()
-                if self.url is not None:
+                self._url = get_stream_url()
+                if self._url is not None:
                     break
             except TimeoutException:
-                log.debug(f'Failed to get stream URL: Attempt #{i + 1}')
+                if i < attempts - 1:
+                    log.debug(f"Failed to get stream URL: Attempt #{i + 1}")
+                else:
+                    log.error(f"Ran out of attempts to get stream url")
+                    raise
 
         super().__init__(*args, **kwargs)
 
     def run(self):
-        with request('GET', self.url, stream=True) as r:
+        with request('GET', self._url, stream=True) as r:
             raw : HTTPResponse = r.raw
 
+            # Dump preroll data
+            with open(devnull, 'wb') as f:
+                f.write(raw.read(self._preroll_len))
+
             while raw.readable():
-                self.byte_buffer.append(raw.read(8192))
+                self._byte_buffer.append(raw.read(8192))
+
+class RadioStreamManager(Thread):
+    _primary_radio_stream : RadioStream = None
+    _redundant_radio_streams : List[RadioStream] = None
+    _radio_stream_lock : RLock = None
+    _desired_redundancy : int = 2
+    _desired_buffer_size : int = 307200
+    _stream_start_attempts : int = 3
+    _sync_seq_length : int = 10000
+    _on_stream_failover_handlers : \
+        List[Callable[[RadioStream, RadioStream], None]] = []
+
+    def __init__(self, redundancy : int = 2, buffer_size : int = 307200, 
+            start_attempts : int = 3, sync_seq_length : int = 10000,
+            on_stream_failover : Callable[[RadioStream, RadioStream], None] = None, 
+            *args, **kwargs) -> None:
+        if redundancy < 1:
+            raise ValueError('Cannot have a redundancy less than 1')
+        
+        if sync_seq_length * 2 > buffer_size:
+            raise ValueError("Not enough room to sync with given sync " + 
+                "sequence length within the given buffer size.")
+
+        self._radio_stream_lock = RLock()
+        self._desired_redundancy = redundancy
+        self._desired_buffer_size = buffer_size
+        self._stream_start_attempts = start_attempts
+        self._sync_seq_length = sync_seq_length
+        
+        if on_stream_failover is not None:
+            self.add_on_stream_failover_handler(on_stream_failover)
+    
+    def _start_new_stream(self) -> RadioStream:
+        result = None
+        while result is None:
+            try:
+                result = RadioStream(self._desired_buffer_size, self._stream_start_attempts,
+                                    daemon=True)
+            except TimeoutException:
+                log.debug("RadioStream failed to start due to TimeoutException. Will try again.")
+        return result
+
+    def _restore_redundancy(self):
+        self._radio_stream_lock.acquire()
+        try:
+            # Prune dead streams
+            for stream in self._redundant_radio_streams:
+                if not stream.is_alive():
+                    self._redundant_radio_streams.remove(stream)
+            
+            # Start up new streams
+            new_streams_needed = self._desired_redundancy - 1 - \
+                                    len(self._redundant_radio_streams)
+            for i in range(0, new_streams_needed):
+                self._redundant_radio_streams.append(self._start_new_stream())
+
+        finally:
+            self._radio_stream_lock.release()
+
+    def _replace_primary_stream(self):
+        with self._radio_stream_lock:
+            failover_stream : RadioStream = None
+            for stream in self._redundant_radio_streams:
+                if stream.is_alive:
+                    failover_stream = stream
+                    break
+            
+            if failover_stream is None:
+                # If all the redundant streams were dead, too, just
+                # make a new stream
+                failover_stream = self._start_new_stream()
+            
+            for handler in self._on_stream_failover_handlers:
+                handler(self._primary_radio_stream, failover_stream)
+            
+            self._primary_radio_stream = failover_stream
+
+    def run(self):
+        event = Event()
+        while True:
+            if not self._primary_radio_stream.is_alive:
+                self._replace_primary_stream()
+            self._restore_redundancy()
+            event.wait(.250)
+    
+    @property
+    def primary_radio_stream(self) -> RadioStream:
+        with self._radio_stream_lock:
+            return self._primary_radio_stream
+  
+    # First param of callable is old stream, second is new
+    def add_on_stream_failover_handler(self, handler : Callable[[RadioStream, RadioStream], None]):
+        if callable(handler) and handler not in self._on_stream_failover_handlers:
+            self._on_stream_failover_handlers.append(handler)
+    
+    def remove_on_stream_failover_handler(self, handler: Callable[[RadioStream, RadioStream], None]):
+        if handler in self._on_stream_failover_handlers:
+            self._on_stream_failover_handlers.remove(handler)
+
+class RedundantRadioStream(Thread):
+    _byte_buffer : ByteBuffer = None
+    _write_lock : RLock = None
+    _radio_stream_manager : RadioStreamManager = None
+    _sync_len : int = 10000
+    _sync_attempts : int = 5
+    _desired_stream_cache_len : int = 200000
+
+    def __init__(self, redundancy : int = 2, buffer_size : int = 307200, 
+            start_attempts : int = 3, sync_len : int = 10000, sync_attempts : int = 5,
+            desired_stream_cache_len : int = 200000, *args, **kwargs) -> None:
+        self._byte_buffer = ByteBuffer(buffer_size)
+        self._write_lock = RLock()
+        self._sync_len = sync_len
+        self._sync_attempts = sync_attempts
+        self._desired_stream_cache_len = desired_stream_cache_len
+
+        self._radio_stream_manager = RadioStreamManager(redundancy, buffer_size, 
+                                        start_attempts, daemon=True)
+
+        super().__init__(self, *args, **kwargs)
+
+    @property
+    def byte_buffer(self):
+        return self._byte_buffer
+    
+    def run(self):
+        if not self._radio_stream_manager.is_alive:
+            self._radio_stream_manager.run()
+
+        # TODO: Read a bit, write it to the main buffer
+        event = Event()
+
+        prs = self._radio_stream_manager.primary_radio_stream
+        with self._write_lock:
+            self.byte_buffer.append(prs.byte_buffer.readUpToRemainingLength(
+                self._desired_stream_cache_len))
+        event.wait(.250)
+
+    def handleFailover(self, old_primary : RadioStream, new_primary : RadioStream) -> None:
+        try:
+            log.debug("Acquiring write lock")
+            self._write_lock.acquire()
+            log.debug("Write lock acquired")
+
+            # Ensure 30% of the desired cache length remains.
+            readLen = int(self._desired_stream_cache_len * 0.3)
+            self.byte_buffer.append(old_primary.byte_buffer.readUpToRemainingLength(readLen))
+
+            syncBytes = self.byte_buffer.readFromEnd(self._sync_len, consume=False)
+
+            # If the new primary doesn't already have the sync bytes in it,
+            # it probably never will. So only try once.
+            successful = False
+            try:
+                new_primary.byte_buffer.seekPastSequence(syncBytes)
+                log.debug("Successfuly synced new primary stream!")
+                successful = True
+            except ValueError:
+                log.debug(f"Failed to sync new primary stream", exc_info=True)
+            
+            if not successful:
+                log.warning(f"Failed to sync new primary stream after {self._sync_attempts} attempts")
+                log.warning("Data will just be appended normally. This may result in a \"jump\" in the recording.")
+
+        finally:
+            log.debug("Releasing write lock")
+            self._write_lock.release()
 
