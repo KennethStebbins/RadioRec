b8de38b235faa621edc53451876f8a1b9604b91c
Kenneth Stebbins <contact@kennethstebbins.com>
2021-10-29 11:51:21 -0600

    Now, after hitting the play button, wait for either the stream to start OR a streaming URL to appear.
    The streaming URL doesn't appear in the performance metrics until the connection is closed.
    That means, if the stream is actually trying to start, we need to wait for it to start, then hit the stop button.
    But if the stream is never going to start (e.g., no compaitible audio codecs or no audio sinks present), then we don't want to wait 30 seconds for no reason.
    In that second case, the stream URL appears in the performance metrics, so we can just check for that at the same time as the normal stream start metrics to avoid waiting for no reason.

--- app/get_stream_url.py
+++ app/get_stream_url.py
@@ -4,6 +4,7 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.firefox.webelement import FirefoxWebElement
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 
@@ -77,11 +78,11 @@ def get_stream_url(page_url : str = 'https://player.listenlive.co/34461', headle
         log.debug("Page loaded.")
 
         # Get all of our buttons
-        btnPlay = WebDriverWait(browser, 10).until(
+        btnPlay : FirefoxWebElement = WebDriverWait(browser, 10).until(
             expected_conditions.presence_of_element_located(btnPlaySelector)
         )
 
-        btnStop = WebDriverWait(browser, 10).until(
+        btnStop : FirefoxWebElement = WebDriverWait(browser, 10).until(
             expected_conditions.presence_of_element_located(btnStopSelector)
         )
         log.debug("Buttons found")
@@ -93,21 +94,21 @@ def get_stream_url(page_url : str = 'https://player.listenlive.co/34461', headle
         log.debug("Play button is now clickable. Clicking...")
         btnPlay.click()
 
-        # Wait until it looks like the stream has started, then click the stop button
-        log.debug("Waiting for stream to start...")
-        try:
-            WebDriverWait(browser, 30).until(
-                stream_has_started
-            )
-            log.debug("Stream started! Clicking stop button...")
+        # Wait to progress. If we find a streaming URL, continue. Otherwise,
+        # wait for the stream to start. When the stream starts, hit the stop
+        # button.
+        log.debug("Waiting for stream to start or streaming URL to appear...")
+        WebDriverWait(browser, 30).until(
+            lambda driver : stream_has_started(driver) or
+                                extract_streaming_url(driver) != ''
+        )
+        if stream_has_started(browser):
+            log.debug("Stream has started! Pressing the stop button...")
             btnStop.click()
-        except TimeoutException:
-            log.debug("It looks like the stream never started. " +
-                "We'll try to extract the stream URL anyway")
 
         # Wait until the streaming URL appears in the browser's performance metrics
         log.debug("Waiting for stream URL to appear in performance metrics...")
-        WebDriverWait(browser, 10).until(
+        WebDriverWait(browser, 5).until(
             lambda driver : extract_streaming_url(driver) != ''
         )
         log.debug("Stream URL found!")
