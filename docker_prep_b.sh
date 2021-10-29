4b1d4062393052f04d702a3ddfb4a069c38aab69
Kenneth Stebbins <contact@kennethstebbins.com>
2021-10-29 10:24:48 -0600

    install pip and prepare the output directory

--- docker_prep_b.sh
+++ docker_prep_b.sh
@@ -2,11 +2,14 @@
 
 cd ~
 
-apt update
-apt install python3 -y
+sudo apt update
+sudo apt install python3 python3-pip -y
 
 # Upgrade pip and install app's Python dependencies
-python -m pip install --upgrade pip
-pip install requests selenium
+python3 -m pip install --upgrade pip
+pip3 install requests selenium
+
+sudo mkdir /usr/local/radiorec/output
+sudo chmod 777 /usr/local/radiorec/output
 
 echo "Done!"
